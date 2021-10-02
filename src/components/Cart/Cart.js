57364fcc08fbc185a85f54f57db16819e66ded5c
Al Boyan <abdullahalboyan16@gmail.com>
2021-10-02 08:38:43 +0600

    first year

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import './Cart.css';
 
 const Cart = (props) => {
     const { cart } = props;
-
+    console.log(props.children)
     // const totalReducer = (previous, product) => previous + product.price;
     // const total = cart.reduce(totalReducer, 0);
     let totalQuantity = 0;
@@ -21,12 +21,32 @@ const Cart = (props) => {
     const grandTotal = total + shipping + tax;
     return (
         <div className="cart">
-            <h3>Order Summary</h3>
-            <h5>Items Ordered: {totalQuantity}</h5>
-            <p>Total: {total.toFixed(2)}</p>
-            <p>Shipping: {shipping}</p>
-            <p>tax: {tax.toFixed(2)}</p>
-            <p>Grand Total: {grandTotal.toFixed(2)}</p>
+            <h3 className="header">Order Summary</h3>
+            <h5 className="header">Items Ordered: {totalQuantity}</h5>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Items</td>
+                        <td>: ${total.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                        <td>Shipping & Handiling</td>
+                        <td>: ${shipping}</td>
+                    </tr>
+                    <tr>
+                        <td>Tax</td>
+                        <td>: ${tax.toFixed(2)}</td>
+                    </tr>
+                    <tr className="grand-total">
+                        <td>Grand total</td>
+                        <td>: ${grandTotal.toFixed(2)}</td>
+                    </tr>
+                </tbody>
+            </table>
+            {
+                props.children
+            }
         </div>
     );
 };
